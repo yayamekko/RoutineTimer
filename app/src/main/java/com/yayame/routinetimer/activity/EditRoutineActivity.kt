1afd89dabcec164c3254d02bb751eb37553e02e3
yayamekko <46895057+yayamekko@users.noreply.github.com>
2020-06-21 23:11:04 +0900

    edit routine 画面のたたき台を作成

---
+++ app/src/main/java/com/yayame/routinetimer/activity/EditRoutineActivity.kt
@@ -0,0 +1,13 @@
+package com.yayame.routinetimer.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yayame.routinetimer.R
+
+class EditRoutineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_routine)
+    }
+}
---
+++ app/src/main/java/com/yayame/routinetimer/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.yayame.routinetimer.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.yayame.routinetimer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // TODO ここを押したらルーチン編集画面に遷移する
+        findViewById<Button>(R.id.edit_routine_button).setOnClickListener {
+            startActivity(Intent(this, EditRoutineActivity::class.java))
+        }
+    }
+}
