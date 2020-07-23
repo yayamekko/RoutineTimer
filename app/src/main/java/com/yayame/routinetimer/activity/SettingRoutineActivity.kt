4509dca399df7f2adda6f4ccdd15e487391d415f
yayamekko <46895057+yayamekko@users.noreply.github.com>
2020-07-23 16:25:34 +0900

    タスク設定画面を作成

---
+++ app/src/main/java/com/yayame/routinetimer/activity/SettingRoutineActivity.kt
@@ -0,0 +1,13 @@
+package com.yayame.routinetimer.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yayame.routinetimer.R
+
+class SettingRoutineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_routine)
+    }
+}
