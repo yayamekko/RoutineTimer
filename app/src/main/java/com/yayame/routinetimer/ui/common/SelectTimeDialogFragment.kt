b98cda66cdd240845fd51d5fbffc2c0d56e4c2ec
yayamekko <46895057+yayamekko@users.noreply.github.com>
2020-10-03 23:19:09 +0900

    時間設定ダイアログのnumberPickerを修正

--- app/src/main/java/com/yayame/routinetimer/ui/common/SelectTimeDialogFragment.kt
+++ app/src/main/java/com/yayame/routinetimer/ui/common/SelectTimeDialogFragment.kt
@@ -1,37 +1,30 @@
 package com.yayame.routinetimer.ui.common
 
+import android.app.AlertDialog
 import android.app.Dialog
-import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.NumberPicker
-import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
 import com.yayame.routinetimer.R
 
-class SelectTimeDialogFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+class SelectTimeDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val inflater = requireActivity().layoutInflater
-        val dialog = TimePickerDialog(
-            activity,
-            this,
-            0,
-            0,
-            true
-        )
 
-        val view = inflater.inflate(R.layout.fragment_select_time_dialog, null)
+        val inflater = requireActivity().layoutInflater
+        val view = inflater.inflate(R.layout.fragment_select_time_dialog, null, false)
         val minutesNumberPicker = view.findViewById<NumberPicker>(R.id.minutes_numberPicker)
         minutesNumberPicker.maxValue = 59
         minutesNumberPicker.minValue = 0
         val secondsNumberPicker = view.findViewById<NumberPicker>(R.id.seconds_numberPicker)
         secondsNumberPicker.maxValue = 59
         secondsNumberPicker.minValue = 0
-        dialog.setView(inflater.inflate(R.layout.fragment_select_time_dialog, null))
-        return dialog
-    }
 
-    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-        // Do something with the time chosen by the user
+        val dialog = AlertDialog.Builder(context).apply {
+            setPositiveButton("OK", null)
+            setNeutralButton("Cancel", null)
+            setView(view)
+        }
+        return dialog.create()
     }
 
     companion object {
